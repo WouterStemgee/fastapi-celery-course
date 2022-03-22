6b47f7137cec27ff65e2cc92a8551b3ebd2d3d3f
Kevin Hou <neonnova916@gmail.com>
2022-03-22 22:31:12 +0800

    websockets and celery

--- project/__init__.py
+++ project/__init__.py
@@ -1,18 +1,34 @@
+from broadcaster import Broadcast
 from fastapi import FastAPI
 
 from project.celery_utils import create_celery
+from project.config import settings
+
+broadcast = Broadcast(settings.WS_MESSAGE_QUEUE)
 
 
 def create_app() -> FastAPI:
     app = FastAPI()
 
     # do this before loading routes
-    # singleton and factory?
+    # factory method
     app.celery_app = create_celery()  # type: ignore
 
     from project.users import users_router
 
     app.include_router(users_router)
+
+    from project.ws import ws_router  # new
+
+    app.include_router(ws_router)
+
+    @app.on_event("startup")
+    async def startup_event():
+        await broadcast.connect()
+
+    @app.on_event("shutdown")
+    async def shutdown_event():
+        await broadcast.disconnect()
 
     @app.get("/")
     async def root():
--- project/config.py
+++ project/config.py
@@ -7,11 +7,21 @@ from functools import lru_cache
 class BaseConfig:
     BASE_DIR: pathlib.Path = pathlib.Path(__file__).parent.parent
 
-    DATABASE_URL: str = os.environ.get("DATABASE_URL", f"sqlite:///{BASE_DIR}/db.sqlite3")
+    DATABASE_URL: str = os.environ.get(
+        "DATABASE_URL", f"sqlite:///{BASE_DIR}/db.sqlite3"
+    )
     DATABASE_CONNECT_DICT: dict = {}
 
-    CELERY_BROKER_URL: str = os.environ.get("CELERY_BROKER_URL", "redis://127.0.0.1:6379/0")
-    CELERY_RESULT_BACKEND: str = os.environ.get("CELERY_RESULT_BACKEND", "redis://127.0.0.1:6379/0")
+    CELERY_BROKER_URL: str = os.environ.get(
+        "CELERY_BROKER_URL", "redis://127.0.0.1:6379/0"
+    )
+    CELERY_RESULT_BACKEND: str = os.environ.get(
+        "CELERY_RESULT_BACKEND", "redis://127.0.0.1:6379/0"
+    )
+
+    WS_MESSAGE_QUEUE: str = os.environ.get(
+        "WS_MESSAGE_QUEUE", "redis://127.0.0.1:6379/0"
+    )
 
 
 class DevelopmentConfig(BaseConfig):
@@ -31,7 +41,7 @@ def get_settings():
     config_cls_dict = {
         "development": DevelopmentConfig,
         "production": ProductionConfig,
-        "testing": TestingConfig
+        "testing": TestingConfig,
     }
 
     config_name = os.environ.get("FASTAPI_CONFIG", "development")
